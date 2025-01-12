a6e49d9d3846b4d06e389050017879bad09a8ae9
Edgar Tormo Jornet <edgtorjor@ale.edu.gva.es>
2025-01-12 22:19:12 +0100

    Commit Twig RetailEdgar

--- src/Controller/IniciController.php
+++ src/Controller/IniciController.php
@@ -3,10 +3,13 @@ namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-class IniciController {
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class IniciController extends AbstractController{
     #[Route('/' ,name:'inici')]
     public function inici() {
-        return new Response("Projecte Gestió Retail de 2n de DAW");
+        //return new Response("Projecte Gestió Retail de 2n de DAW");
+        return $this->render('base.html.twig');
     }
 }
 ?>
